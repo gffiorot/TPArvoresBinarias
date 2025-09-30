33e94a4452ee5e14f9966edf696ff75ac0f75667
Gustavo Firmem <gffiorot@outlook.com>
2025-09-30 15:58:53 -0300

    Commit inicial

---
+++ src/app/comparators/ComparadorAlunoPorNome.java
@@ -0,0 +1,20 @@
+package app.comparators;
+
+import java.util.Comparator;
+import app.Aluno;
+/**
+ *
+ * @author victoriocarvalho
+ *
+ * Essa é comparadora de alunos por nome que será utilizada para criar as árvores
+ * nos programas de teste para redigir os relatórios.
+ */
+
+public class ComparadorAlunoPorNome implements Comparator<Aluno> {
+
+    @Override
+    public int compare(Aluno o1, Aluno o2) {
+        return o1.getNome().compareTo(o2.getNome());
+    }
+
+}
---
+++ src/lib/ArvoreBinaria.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ *
+ * @author victoriocarvalho
+ */
+public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
+
+    private class No<T>{
+        private T valor;
+        private No<T> filhoEsquerdo;
+        private No<T> filhoDireito;
+
+        public No(T valor) {
+            this.valor = valor;
+            this.filhoEsquerdo = null;
+            this.filhoDireito = null;
+        }
+    }
+
+    protected No<T> raiz = null;
+    protected Comparator<T> comparador;
+
+    public ArvoreBinaria(Comparator<T> comp) {
+        this.comparador = comp;
+        this.raiz = null;
+    }
+
+    @Override
+    public void adicionar(T novoValor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T pesquisar(T valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T pesquisar(T valor, Comparator comparador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T remover(T valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int altura() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    @Override
+    public int quantidadeNos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String caminharEmNivel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.    
+    }
+
+    @Override
+    public String caminharEmOrdem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.    
+    }
+
+}
